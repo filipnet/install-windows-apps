773eb01943087b3e73ab324c25f601bdf0b31c6e
Benedikt Filip <benedikt.filip@filipnet.de>
2022-12-06 08:10:29 +0100

    Comparison with installed software

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # install-windows-apps
+
 Use Chocolatey to automate application installations on a Windows DevOps machine
+
+## LICENSE
+
+install-windows-apps and all individual scripts are under the BSD 3-Clause license unless explicitly noted otherwise. Please refer to the LICENSE
--- install-windows-apps.ps1
+++ install-windows-apps.ps1
@@ -1,72 +1,127 @@
-:: Install choco .exe and add choco to PATH
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+## Install choco .exe and add choco to PATH
+[System.Net.ServicePointManager]::SecurityProtocol = 3072
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-:: Install all the packages
-:::: Browsers
+choco feature enable -n allowGlobalConfirmation
+
+## Browsers
 choco install googlechrome -y
 choco install firefox -y
-choco install chromium -y
-choco install googlechrome.canary -y
-choco install brave -y
-choco install opera -y
-choco install tor-browser -y
+# choco install chromium -y
+# choco install opera -y
 
-:::: Dev tools
+## Dev tools
 # choco install cygwin -y
 choco install wireshark -y
-choco install git -y
-choco install sourcetree -y
+choco install git.install -y
+#choco install sourcetree -y
 choco install jre8 -y
-choco install postman -y
-choco install docker-compose -y
+#choco install postman -y
+#choco install docker-compose -y
+choco install mqtt-explorer -y
+choco install openssl -y
 
-:: Server tools
+## Server tools
 choco install putty.install -y
 choco install winscp.install -y
 choco install filezilla -y
+choco install nmap -y
+choco install heidisql -y
 
-:: Package managers
-# choco install yarn -y
-# choco install nvm -y
+## Environments
+choco install powershell -y
+choco install jre8 -y
+choco install vcredist140 -y
+choco install dotnet3.5 -y
 
-:: Task managers
+## System management tools
 choco install procmon -y
-choco install procexp -y
+choco install choco install opensslchoco install opensslprocexp -y
 choco install processhacker.install -y
+choco install sysinternals -y
+# choco install bginfo -y
 
-:: VM
+## VM
 # choco install vmware-workstation-player -y
 # choco install virtualbox -y
 
-:: Text editors / IDEs / Readers
-# choco install adobereader -y
-# choco install pdfcreator -y
-# choco install sublimetext3 -y
-choco install intellijidea-ultimate -y
+## Text editors and development tools
+choco install notepadplusplus.install -y
 choco install visualstudiocode -y
 # choco install visualstudio2019community -y
 # choco install androidstudio -y
-# choco install anaconda3 -y
 choco install arduino -y
 
-:::: Media
-choco install vlc -y
-# choco install spotify -y
- 
-:: Utilities 
-# choco install displayfusion -y
-# choco install ccleaner -y
-# choco install google-backup-and-sync -y
-# choco install google-drive-file-stream -y
+## Office
+choco install libreoffice-fresh -y
+choco install thunderbird -y
+# choco install adobereader -y
+choco install foxitreader -y
+choco install pdfsam.install -y
+choco install kindle -y
+choco install freemind -y
 # choco install office365proplus -y
+
+## Collaboration
+choco install microsoft-teams.install -y
+choco install webex-meetings -y
+choco install gotomeeting -y
+choco install zoom -y
+# choco install skype -y
+# choco install dropbox -y
+choco install nextcloud-client -y
+choco install jabra-direct -y
+choco install phonerlite -y
+choco install teamviewer -y
+
+## Media
+choco install vlc -y
+choco install deezer -y
+# choco install spotify -y
+# choco install gimp -y
+choco install audacity -y
+choco install obs-studio.install -y
+choco install drawio -y
+choco install shotcut.install -y
+choco install eac -y
+choco install fsviewer -y
 choco install obs-studio -y
+
+## Messanger
+choco install whatsapp -y
+choco install telegram.install -y
+
+## Virtual private network
+choco install openvpn -y
+choco install wireguard -y
+
+## Productivity and utilities
+choco install freecommander-xe.install -y
+choco install totalcommander -y
+# choco install ccleaner -y
 # choco install partitionwizard -y
 choco install rufus -y
-# choco install sharex -y
-# choco install slack -y
-choco install skype -y
-choco install teamviewer -y
+choco install windirstat -y
 choco install treesizefree -y
-choco install winrar -y
-# choco install xmind -y
-choco install zoom -y
+# choco install winrar -y
+# choco install 7zip -y
+choco install peazip -y
+choco install rufus -y
+choco install burnawarefree -y
+choco install deepl -y
+choco install dexpot -y
+choco install greenshot -y
+
+## Security
+choco install keepass -y
+choco install winauth -y
+
+## Gaming
+choco install steam -y
+choco install origin -y
+
+## Children
+choco install tiptoimanager -y
+
+choco feature disable -n allowGlobalConfirmation
